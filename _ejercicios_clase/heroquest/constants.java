4089c716c3f91d7f16d0decc05830628737726c0
Alberto Sancho <designalbertosancho@gmail.com>
2025-01-09 00:35:55 +0100

    heroquest_final

--- _ejercicios_clase/heroquest/constants.java
+++ _ejercicios_clase/heroquest/constants.java
@@ -2,20 +2,20 @@ package heroquest;
 
 import java.util.Scanner;
 
-public class settings {
-
+public class constants {
   final static Scanner KEYBOARD = new Scanner(System.in);
   final static int INITIAL_ENERGY = 100;
+  final static int MIN_ENERGY = 30;
   final static byte FOUNDED_COINS = 10;
   final static byte TRAP_REVEALED = -15;
   final static byte STARTING_DUNGEON = 1;
   final static byte FINAL_DUNGEON = 3;
-
   final static byte[][] DUNGEON_PATHS = {
       {2,3},
       {1,4},
-      {},
+      {1,1},
       {1,2}
   };
-
+  final static String ANSI_BOLD = "\033[1m";
+  final static String ANSI_RESET = "\033[0m";
 }
--- _ejercicios_clase/heroquest/heroquest.java
+++ _ejercicios_clase/heroquest/heroquest.java
@@ -2,6 +2,13 @@ package heroquest;
 
 public class heroquest {
     public static void main(String[] args) {
+       
+        // Variables iniciales
+        boolean playGame = true;
+        byte startingDungeon = constants.STARTING_DUNGEON, initialEnergy = constants.INITIAL_ENERGY, initialTurn = 1, playerOption = -1; //defino las variables iniciales
+        byte[] currentValues = {startingDungeon, initialEnergy, initialTurn, playerOption}; //creo un array con las variables iniciales que se irá modificando a lo largo de la partida
+        byte[] dungeonEnergy = getDungeonEnergy(); // Se obtienen los valores de energía para cada mazmorra.
+        
         // Welcome message
         System.out.println("*****************************************************");
         System.out.println("*                                                   *");
@@ -9,41 +16,50 @@ public class heroquest {
         System.out.println("*                                                   *");
         System.out.println("*****************************************************");
         System.out.println("""
-            El objetivo de este juego es avanzar por las distintas mazmorras hasta llegar a la mazmorra 3 con el máximo número de puntos de vida. 
-            Al avanzar por las salas se desencadenarán distintos eventos, tanto positivos como negativos que pondrán en peligro tu misión.
-            ¡Buena suerte!
+            El objetivo del juego es avanzar por las distintas mazmorras hasta llegar a la mazmorra final con al menos 30 puntos de vida. 
+            Al avanzar por las salas se desencadenarán distintos eventos, tanto positivos como negativos, que pondrán en peligro tu misión.
+            ¡Buena suerte, aventurero!.
             """);
-        
-        boolean playGame = true;
-        byte startingDungeon = settings.STARTING_DUNGEON, initialEnergy = settings.INITIAL_ENERGY, initialTurn = 1, playerOption = -1;
-        byte[] currentValues = {startingDungeon, initialEnergy, initialTurn, playerOption};
-        byte[] dungeonEnergy = getDungeonEnergy();
 
         while (playGame) {
             if (currentValues[1] <= 0) {
-                System.out.println("Vaya... te has quedado más tieso que la mojama. Inténtalo de nuevo");
+                System.out.println("GAME OVER... te has quedado más tieso que la mojama. Inténtalo de nuevo.\n");
                 playGame = false;
             }
             else {
-                if (currentValues[0] == settings.FINAL_DUNGEON) {
-                    System.out.println("Enhorabuena aventurero, has llegado al final de tu aventura.");
+                if (currentValues[0] == constants.FINAL_DUNGEON && currentValues[1] >= constants.MIN_ENERGY) {
+                    System.out.print("""
+                    ¡¡¡Enhorabuena!!!
+                    Has llegado a la mazmorra final con energía suficiente para completar la aventura.
+                    Estas son tus estadísticas de juego:
+                    """);
+                    System.out.println("-".repeat(30));
+                    System.out.printf("%-30s %s%n", constants.ANSI_BOLD + "Energía restante:" + constants.ANSI_RESET, currentValues[1]);
+                    System.out.printf("%-30s %s%n", constants.ANSI_BOLD + "Turnos empleados:" + constants.ANSI_RESET, currentValues[2] - 1);
+                    System.out.println("-".repeat(30));
+
                     playGame = false;
                 }
                 else if (currentValues[3] == 4) {
-                    System.out.println("Gracias por jugar a Heroquest, aventurero. Vuelve siempre que quieras.");
+                    System.out.println("\nGracias por jugar a Heroquest, aventurero. Vuelve siempre que quieras.\n");
                     playGame = false;
                 }
                 else {
+                    System.out.printf("TURNO %d: \n", currentValues[2]);
+                    System.out.println("========");
+                    System.out.printf("Estas en la mazmorra %s y tu energía actual es de %d puntos.\n\n", currentValues[0], currentValues[1]);
                     playerOptions(currentValues, dungeonEnergy);
+                    currentValues[2]++;
                 }
             }
         }
         
     }
 
-    public static byte[] playerOptions(byte[] currentValues, byte[] dungeonEnergy) {
-        System.out.printf("TURNO %d: \n", currentValues[2]);
-        System.out.printf("Estas en la mazmorra %s y tu energía actual es de %d puntos. \n", currentValues[0], currentValues[1]);
+    /* Muestra las opciones del menú principal y actualiza los valores del juego en función de la elección.
+     * @return array
+     */
+    private static byte[] playerOptions(byte[] currentValues, byte[] dungeonEnergy) {
         System.out.println("""
             Por favor elije una de las siguientes opciones:
             
@@ -52,26 +68,27 @@ public class heroquest {
             3. Inspeccionar la energía de las mazmorras.
             4. Abandona el juego.""");
             
-        byte option = validateOption((byte) 1, (byte) 4);
+        byte option = validateOption((byte) 1, (byte) 4); //Se valida la opción elegida
 
         switch (option) {
             case 1:
                 chooseDungeon(currentValues, dungeonEnergy);
                 break;
             case 2:
-                System.out.println("Cuidadín!");
+                //System.out.println("Cuidadín! no siempre suceden cosas buenas");
+                inspectDungeon(currentValues);
                 break;
             case 3:
-                System.out.println("Estos son los valores de energía de cada Mazmorra:");
+                System.out.println("\nEstos son los valores de energía de cada Mazmorra:");
                 for (int i = 0; i < dungeonEnergy.length; i++) {
                     System.out.printf("Mazmorra %d: %d puntos de energía\n", i+1, dungeonEnergy[i]); 
                 }
+                System.out.println("\n");
                 break;
             case 4:
                 break;
         }
 
-        currentValues[2]++;
         currentValues[3] = option;
 
         return currentValues;
@@ -84,45 +101,71 @@ public class heroquest {
 
         byte currentDungeon = currentValues[0];
 
-        System.err.println("Elije la mazmorra a la que quieres entrar: ");
-
         switch (currentDungeon) {
             case 1:
+                System.err.println("\nElije la mazmorra a la que quieres entrar (introduce el valor 1 o 2 para cada caso): ");
                 System.out.println("""
                     1. Mazmorra 2.
                     2. Mazmorra 3.""");
                 break;
             case 2:
+                System.err.println("\nElije la mazmorra a la que quieres entrar (introduce el valor 1 o 2 para cada caso): ");
                 System.out.println("""
                     1. Mazmorra 1.
                     2. Mazmorra 4.""");
                 break;
             case 3:
                 System.out.println("""
-                    Estas en la Mazmorra final.""");
+                    \nVaya, estás en la Mazmorra final pero necesitas al menos 30 puntos de energía para terminar el juego.
+                    Introduce 1 o 2 para volver a la Mazmorra inicial.""");
                 break;
             case 4:
+                System.err.println("\nElije la mazmorra a la que quieres entrar (introduce el valor 1 o 2 para cada caso): ");
                 System.out.println("""
                     1. Mazmorra 1.
                     2. Mazmorra 2.""");
                 break;
         }
 
-        byte option = validateOption((byte) 1, (byte) 2);
+        byte option = validateOption((byte) 1, (byte) 2); //Se valida la opción elegida
 
         if (option != 3) {
 
-          currentValues[0] = settings.DUNGEON_PATHS[currentDungeon - 1][option - 1]; // Se modifica el valor de la sala actual
+          currentValues[0] = constants.DUNGEON_PATHS[currentDungeon - 1][option - 1]; // Se modifica el valor de la sala actual
           byte energyModifier = dungeonEnergy[currentValues[0] -1];
   
           if (energyModifier > 0) {
-              System.err.printf("Estás de suerte aventurero, acabas de ganar %d puntos de energía.\n", energyModifier );
+              System.err.printf("Estás de suerte aventurero, %sacabas de ganar %d puntos de energía%s.\n\n", constants.ANSI_BOLD, energyModifier, constants.ANSI_RESET );
           }
           else if (energyModifier < 0) {
-              System.err.printf("Lo siento aventurero, acabas de perder %d puntos de energía.\n", energyModifier );
+              System.err.printf("Lo siento aventurero, %sacabas de perder %d puntos de energía%s.\n\n", constants.ANSI_BOLD, energyModifier, constants.ANSI_RESET );
           }
   
           currentValues[1] = (byte) (currentValues[1] + energyModifier); // Se modifica el valor actual de la energía
+        }
+
+        return currentValues;
+    }
+
+    /* Determina el evento que sucede al inspeccionar una sala y actualiza los valores del juego en función del mismo.
+     * @return array
+     */
+    private static byte[] inspectDungeon(byte[] currentValues) {
+
+        byte randomEvent = (byte) ((Math.random() * 3) + 1);
+
+        switch (randomEvent) {
+            case 1:
+                System.out.printf("¡Estás de enhorabuena aventurero!, %shas encontrado un puñado de monedas que te dan %d puntos de energía%s.\n\n",  constants.ANSI_BOLD, constants.FOUNDED_COINS, constants.ANSI_RESET);
+                currentValues[1] = (byte) (currentValues[1] + constants.FOUNDED_COINS);
+                break;
+            case 2:
+                System.out.printf("¡Mala suerte!, %sen tu afán de explorar has caído en una trampa que te quita %d de energía%s.\n\n", constants.ANSI_BOLD, constants.TRAP_REVEALED, constants.ANSI_RESET);
+                currentValues[1] = (byte) (currentValues[1] + constants.TRAP_REVEALED);
+                break;
+            case 3:
+                System.out.printf("Lo sentimos, en esta sala no has encontrado nada... por el momento.\n");
+                break;
         }
 
         return currentValues;
@@ -137,8 +180,8 @@ public class heroquest {
 
         // Validar la entrada
         while (!entradaValida) {
-            if (settings.KEYBOARD.hasNextByte()) {
-                value = settings.KEYBOARD.nextByte();
+            if (constants.KEYBOARD.hasNextByte()) {
+                value = constants.KEYBOARD.nextByte();
                 if (value >= min && value <= max) {
                     entradaValida = true;  // Entrada válida
                 } else {
@@ -146,7 +189,7 @@ public class heroquest {
                 }
             } else {
                 System.out.println("Entrada no válida. Debe ingresar un número entero.");
-                settings.KEYBOARD.next();
+                constants.KEYBOARD.next();
                 return validateOption(min, max);
             }
         }
@@ -169,5 +212,3 @@ public class heroquest {
      }
 
 }
-
-
